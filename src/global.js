36ab8a1409128e874a528ef95eee2c207e6cca8a
RaghuRamAnnamdasu <annamdasuraghuram@gmail.com>
2022-12-14 20:37:31 +0530

    Added render deployed backend url

--- src/global.js
+++ src/global.js
@@ -1,4 +1,5 @@
 // export const API = "http://localhost:4000";
 
-export const API = "https://dummy-student-dashboard.herokuapp.com"
+// export const API = "https://dummy-student-dashboard.herokuapp.com"
 
+export const API = "https://student-dashboard-backend.onrender.com";
